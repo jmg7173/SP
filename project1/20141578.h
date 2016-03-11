53368b6b85f78e7ae1f4b562ffac120dfeb9b788
JMG <jmg7173@sogang.ac.kr>
2016-03-11 21:38:24 +0900

    minor header fix

--- project1/20141578.h
+++ project1/20141578.h
@@ -1,9 +1,6 @@
 #ifndef _20141578_H
 #define _20141578_H
 
-#define HELP_COMMAND_NUM 10
-#define MAX_COMMAND_LENGTH 511
-
 void quit_program();
 void delete_trailing_whitespace(char*);
 int command_with_whitespace(char*);
