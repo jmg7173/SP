195573dc65cf87245ddac99d948fb6779329fe32
Ubuntu <ubuntu@ip-172-31-10-171.ap-northeast-2.compute.internal>
2016-05-22 06:43:56 +0000

    Project 5 completed.

---
+++ project5/mapper.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from sys import stdin, stdout
+
+while True:
+    str1 = stdin.readline().lower()
+    if not str1:
+        break
+    prev = ""
+    for word in str1.split(): 
+        if prev:
+            stdout.write("%s\t%s\t1\n" % (prev,word))
+        prev = word
---
+++ project5/reducer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+from sys import stdin, stdout
+
+words = {}
+while True:
+    str1 = stdin.readline()
+    if not str1:
+        break
+    splited = str1.split()
+    key, word, count = splited[0], splited[1], splited[2]
+    if key not in words:
+        words[key] = {}
+    
+    if word in words[key]:
+        words[key][word] += 1
+    else:
+        words[key][word] = 1
+
+for keys in sorted(words):
+    for word in sorted(words[keys]):
+        stdout.write("%s\t%s\t%d\n" % (keys, word, words[keys][word]))    
