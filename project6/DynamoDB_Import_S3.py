0b4071e20bcdbf313680d2731e65385ef89b54ce
jmg7173 <jmg7173@sogang.ac.kr>
2016-06-07 06:51:06 +0000

    comment added.

--- project6/DynamoDB_Import_S3.py
+++ project6/DynamoDB_Import_S3.py
@@ -4,18 +4,28 @@ from boto.dynamodb2.table import Table
 from boto.dynamodb2.exceptions import ConditionalCheckFailedException as CCException
 from boto.s3.connection import S3Connection
 s3 = S3Connection()
+
+# Get my bucket
 bucket = s3.get_bucket('sp20141578proj6')
 
+# Get DynamoDB table
 dynamoTable = Table('project6')
+
+# Get file lists from bucket
 temp = bucket.list()
 for i in temp:
+    # Get part-0000X files
     if 'project5output' in i.key and 'part-' in i.key:
+        # Get file contents
         content = i.get_contents_as_string()
+        # remove last line(EOF)
         for line in content.split('\n')[:-1]:
+            # If word already exists, ConditionalCheckFailedException raise.
             try:
                 words, counts = line.split('\t')
                 dynamoTable.put_item(data={'words':words, 'count':counts})
             except CCException:
+                # If word already exists, renew count.
                 exist = dynamoTable.get_item(words=words)
                 exist['count'] = int(exist['count']) + int(counts)
                 exist.save()
--- project6/DynamoDB_Query_CLI.py
+++ project6/DynamoDB_Query_CLI.py
@@ -2,21 +2,30 @@ execfile('20141578.conf')
 
 from boto.dynamodb2.table import Table
 from boto.dynamodb2.exceptions import ItemNotFound
-myTable = Table('project6')  # TABLE NAME HERE
+myTable = Table('project6')
 
 # get item
 while(True):
+    # Get input
     key = raw_input("Insert words(If you want to quit, input 'quit') : ").strip()
+    
+    # quit program
     if(key == 'quit'):
         break
+
+    # blank string
     elif not key:
         print "Do not input blank word."
+
     else:
         try:
+            # Remove trailing whitespace.
             first, second = key.split()
             input_word = first+" "+second
-            temp = myTable.get_item(words=input_word)  # PRIMARY KEY HERE
+            # Get information from DynamoDB
+            temp = myTable.get_item(words=input_word)
             print "words: "+ temp['words']
             print "count: " + str(temp['count'])
+        # exception handling for ItemNotFound and one word input
         except (ItemNotFound, ValueError) as e:
             print "words '%s' is invalid key." % key
